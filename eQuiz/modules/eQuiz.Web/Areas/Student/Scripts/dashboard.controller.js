60813f18f5a767f44fbc66c167c81f82516ce0ba
Maryan Mayher <mr.maykher@gmail.com>
2016-05-15 20:42:07 +0300

    Added dashboard.service.js

--- eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/dashboard.controller.js
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/dashboard.controller.js
@@ -2,12 +2,15 @@
 (function (angular) {
     var equizModule = angular.module("equizModule");
 
-    equizModule.controller("dashboardCtrl", ["$scope", "$http", function ($scope, $http) {
+    equizModule.controller("dashboardCtrl", ["$scope", "$http", "dashboardService", function ($scope, $http, dashboardService) {
         $scope.allQuizes = null;
 
-        $http.get("GetQuizes").success(function (data) {
-            $scope.allQuizes = data;
-            console.log($scope.allQuizes);
-        });
+        activate();
+        function activate() {
+            var quizPromise = dashboardService.getQuizzes();
+            quizPromise.success(function (data) {
+                $scope.allQuizes = data;
+            });
+        };
     }]);
 })(angular);
---
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/dashboard.service.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="E:\MyFork_eQuiz\eQuiz\modules\eQuiz.Web\Scripts/libs/angularjs/angular.js" />
+(function (angular) {
+    var equizModule = angular.module("equizModule");
+
+    equizModule.factory("dashboardService", ["$http", function ($http) {
+
+        var service = {
+            getQuizzes: getQuizzesAjax
+        };
+
+        return service;
+
+        function getQuizzesAjax() {
+            var promise = $http.get("GetQuizes");
+            return promise;
+        };
+    }]);
+})(angular);
