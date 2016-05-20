85eaea5c64039734d933899dab2973d9f982a7be
Oleksii Shevchuk <oleksii.shevchuk.n@gmail.com>
2016-05-20 10:59:50 +0300

    Added Timer

--- eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/quizInRun.controller.js
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/quizInRun.controller.js
@@ -2,7 +2,8 @@
 (function (angular) {
     var equizModule = angular.module("equizModule");
 
-    equizModule.controller("quizInRunCtrl", ["$scope", "$http", "$routeParams", function ($scope, $http, $routeParams) {
+    equizModule.controller("quizInRunCtrl", ["$scope", "$http", "$routeParams", "$interval",
+        function ($scope, $http, $routeParams, $interval) {
         $scope.quizQuestions = null;
         $scope.quizId = $routeParams.id;
         $scope.quizDuration = $routeParams.dura;
@@ -32,6 +33,72 @@
                 $scope.quizQuestions = response.data;
             });
         };
+
+        /////////////////////////////////TIMER
+        $scope.tSeconds = 0;
+        $scope.tMinutes = $scope.quizDuration;
+
+        $scope.timer_1 = $scope.tSeconds;
+        $scope.minutes = $scope.tMinutes;
+        $scope.myStyle = {};
+        var stop;
+
+
+        $scope.start = function () {
+            if (angular.isDefined(stop)) return;
+
+            stop = $interval(function () {
+
+                if ($scope.timer_1 > 0) {
+                    $scope.timer_1 = $scope.timer_1 - 1;
+                } else if ($scope.minutes > 0) {
+                    $scope.minutes = $scope.minutes - 1;
+                    $scope.timer_1 = 59;
+                } else {
+                    $scope.Stop();
+                }
+                if ($scope.minutes <= 119) {
+                    $scope.myStyle = {
+                        color: 'red'
+                    }
+                } else {
+                    $scope.myStyle = {
+                        color: 'black'
+                    }
+                }
+            }, 1000);
+        };//start timer
+
+        $scope.stop = function () {
+            if (angular.isDefined(stop)) {
+                $interval.cancel(stop);
+                stop = undefined;
+            }
+        };
+
+        $scope.reset = function () {
+            $scope.Stop();
+            if ($scope.tSeconds >= 60 && $scope.tSeconds < 0) {
+                $scope.tSeconds = 0;
+            }
+
+            if ($scope.tMinutes < 0) {
+                $scope.tMinutes = 0;
+            }
+
+            $scope.timer_1 = $scope.tSeconds;
+            $scope.minutes = $scope.tMinutes;
+            $scope.myStyle = {
+                color: 'black'
+            }
+        };
+
+        $scope.$on('$destroy', function () {
+            $scope.Stop();
+        });
+
+
+
 
     }]);
 })(angular);
