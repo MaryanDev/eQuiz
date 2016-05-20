a707f5f6e01e6b570608832f0103fc0ab0edbdf4
Здебський Петро <petroZd@gmail.com>
2016-05-20 12:28:42 +0300

    Add AnswerResult and QuizResult classes

---
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Models/AnswerResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eQuiz.Web.Areas.Student.Models
+{
+    public class AnswerResult
+    {
+        public int QuestionId;
+        public int? AnswerId;
+        public bool IsAutomatic;
+        public string AnswerText;
+        public DateTime AnswerTime;
+        public int QuizBlock;
+    }
+}
---
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Models/QuizResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eQuiz.Web.Areas.Student.Models
+{
+    public class QuizResult
+    {
+        public int QuizId;
+        public List<AnswerResult> AnswerResults;
+        public DateTime StartDate;
+        public DateTime FinishDate;
+    }
+}
