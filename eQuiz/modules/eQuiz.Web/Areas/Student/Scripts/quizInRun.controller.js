6a0c696e23529876f54583e23e76ff905a052d61
Здебський Петро <petroZd@gmail.com>
2016-05-21 15:06:43 +0300

    Revert "Extract timer in own service"
    This reverts commit 95aecbface274aeaa3e3875d42e02a8c0d554da8.

--- eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/quizInRun.controller.js
+++ eQuiz/modules/eQuiz.Web/Areas/Student/Scripts/quizInRun.controller.js
@@ -2,8 +2,8 @@
 (function (angular) {
     var equizModule = angular.module("equizModule");
 
-    equizModule.controller("quizInRunCtrl", ["$scope", "$http", "$routeParams", "timerService",
-        function ($scope, $http, $routeParams, timerService) {
+    equizModule.controller("quizInRunCtrl", ["$scope", "$http", "$routeParams", "$interval",
+        function ($scope, $http, $routeParams, $interval) {
             $scope.quizQuestions = null;
 
             $scope.quizId = $routeParams.id;
@@ -48,7 +48,7 @@
                 //console.log($scope.finalUserResult.answerResult.length);
             };
 
-            // Timer
+            /////////////////////////////////TIMER
             $scope.tSeconds = 0;
             $scope.tMinutes = $scope.quizDuration;
 
@@ -57,15 +57,62 @@
             $scope.myStyle = {};
             var stop;
 
+
             $scope.start = function () {
-                timerService.start();
-            }
+                if (angular.isDefined(stop)) return;
+
+                stop = $interval(function () {
+
+                    if ($scope.timer_1 > 0) {
+                        $scope.timer_1 = $scope.timer_1 - 1;
+                    } else if ($scope.minutes > 0) {
+                        $scope.minutes = $scope.minutes - 1;
+                        $scope.timer_1 = 59;
+                    } else {
+                        $scope.Stop();
+                    }
+                    if ($scope.minutes <= 119) {
+                        $scope.myStyle = {
+                            color: 'red'
+                        }
+                    } else {
+                        $scope.myStyle = {
+                            color: 'black'
+                        }
+                    }
+                }, 1000);
+            };//start timer
+
             $scope.stop = function () {
-                timerService.stop();
-            }
+                if (angular.isDefined(stop)) {
+                    $interval.cancel(stop);
+                    stop = undefined;
+                }
+            };
+
             $scope.reset = function () {
-                timerService.reset();
-            }
+                $scope.Stop();
+                if ($scope.tSeconds >= 60 && $scope.tSeconds < 0) {
+                    $scope.tSeconds = 0;
+                }
+
+                if ($scope.tMinutes < 0) {
+                    $scope.tMinutes = 0;
+                }
+
+                $scope.timer_1 = $scope.tSeconds;
+                $scope.minutes = $scope.tMinutes;
+                $scope.myStyle = {
+                    color: 'black'
+                }
+            };
+
+            $scope.$on('$destroy', function () {
+                $scope.Stop();
+            });
+
+
+
 
         }]);
 })(angular);
